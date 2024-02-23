14d5756c4e3a9028d89e6f5f4acc4c43770bac49
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2024-02-23 18:44:45 +0800

    fix sth

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'tailwindcss-universal-colors/',
+  base: '/tailwindcss-universal-colors/',
   plugins: [react()],
   resolve: {
     alias: {
